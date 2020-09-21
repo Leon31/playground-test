5c3bb2bf0bd754e3d48ea200a9ed93f5e6abc3f6
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2020-09-21 17:15:42 +0200

    feat: layour header

---
+++ src/layout/header/header.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useRef, useEffect } from 'react';
+import logo from '../../assets/images/logo-playground-white.png';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+function Header () {
+  const [pageScroll, setPageScroll] = useState(false);
+  const navRef = useRef<Boolean>();
+  
+  navRef.current = pageScroll
+  useEffect(() => {
+    const handleScroll = () => {
+      const show = window.scrollY > 100
+      if (navRef.current !== show) {
+        setPageScroll(show)
+      }
+    }
+    document.addEventListener('scroll', handleScroll)
+    return () => {
+      document.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <div className={pageScroll ? "navbar navbar--scroll" : "navbar"}>
+      <div className="navbar_logo">
+        <img src={logo} alt="Playground logo"/>
+      </div>
+      <div className="navbar_menu">
+        <AnchorLink offset='70' href="#we-are" className="menu_item">we are</AnchorLink>
+        <AnchorLink offset='70' href="#we-do" className="menu_item">we do</AnchorLink>
+        <AnchorLink offset='70' href="#careers" className="menu_item">careers</AnchorLink>
+        <AnchorLink offset='70' href="#contact-us" className="menu_item">contact us</AnchorLink>
+      </div>
+    </div>
+  );
+}
+
+export default Header
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './header';
 
 function Layout () {
   return (
     <div className="layout">
+      <Header/>
     </div>
   );
 }
