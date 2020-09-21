c7d1e8b27e632225a4a35978263f539f936b956c
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2020-09-21 17:17:42 +0200

    feat: footer and cookies

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './header';
+import Cookies from './cookies';
+import Footer from './footer';
+
 
 function Layout () {
   return (
     <div className="layout">
       <Header/>
+      <Cookies/>
+      <Footer/>
     </div>
   );
 }
