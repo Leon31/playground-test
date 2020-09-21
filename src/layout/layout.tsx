0891a539137256b481dc40e53d30bc5977f1de27
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2020-09-21 17:19:39 +0200

    feat: add main page

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -3,11 +3,13 @@ import Header from './header';
 import Cookies from './cookies';
 import Footer from './footer';
 
+import Main from '../pages/main';
 
 function Layout () {
   return (
     <div className="layout">
       <Header/>
+      <Main/>
       <Cookies/>
       <Footer/>
     </div>
