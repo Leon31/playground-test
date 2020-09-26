9b40e5e971c372e4d749fbe5a67aa47336a0e24b
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2020-09-26 17:49:13 +0200

    feat: completed footer

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+import logo from '../../assets/images/logo-playground-secondary.png';
 
 function Footer () {
   return (
     <div className="footer">
+      <div className="footer_logo">
+        <img src={logo} alt="Playground logo secondary"/>
+      </div>
+      <div className="footer_menu">
+        <AnchorLink offset='70' href="#we-are" className="menu_item">we are</AnchorLink>
+        <AnchorLink offset='70' href="#we-do" className="menu_item">we do</AnchorLink>
+        <AnchorLink offset='70' href="#careers" className="menu_item">careers</AnchorLink>
+        <AnchorLink offset='70' href="#contact-us" className="menu_item">contact us</AnchorLink>
+      </div>
+      <div className="footer_social">
+        <span className="fab fa-facebook-square"> </span>
+        <span className="fab fa-linkedin-in"> </span>
+        <span className="fab fa-instagram"> </span>
+      </div>
     </div>
   );
 }
